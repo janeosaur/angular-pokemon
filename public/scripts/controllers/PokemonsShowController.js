82026cb72cf4c1adcb06404e73367d5cf3aeab96
janeosaur <janewie@gmail.com>
2017-04-27 11:05:37 -0700

    saving progress

--- public/scripts/controllers/PokemonsShowController.js
+++ public/scripts/controllers/PokemonsShowController.js
@@ -24,14 +24,30 @@ function PokemonsShowController($http, $routeParams, $location) {
   vm.deletePokemon = function (pokemon) {
     $http({
       method: 'delete',
-      url: 'https://super-crud.herokuapp.com/pokemon/' + $routeParams.id
+      url: 'https://super-crud.herokuapp.com/pokemon/' + pokemon._id
     }).then(function successCallback(response) {
       // var index = vm.pokemon.indexOf(pokemon);
       // vm.pokemon.splice(index, 1); // are these not needed bc one pokemon is already singled out?
-      $location.path('/'); // location is undefined
+      $location.path('/'); 
       console.log('success deleting ', response)
     }, function errorCallback(response) {
       console.log('error deleting ', response);
+    });
+  }
+
+  vm.editPokemon = function (pokemon) {
+    $http({
+      method: 'put',
+      url: 'https://super-crud.herokuapp.com/pokemon/' + pokemon._id,
+      data: {
+        pokedex: pokemon.pokedex,
+        evolves_from: pokemon.evolves_from,
+        image: pokemon.image
+      }
+    }).then(function successCallback(response) {
+      console.log('success editing', response)
+    }, function errorCallback(error) {
+      console.log('error editing ', error);
     });
   }
 
